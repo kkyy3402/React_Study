4f25e224ea4361c57cabfa347610422a6e455f5c
gangyeolkim <kkyy3402@naver.com>
2021-01-06 19:13:45 +0900

    스타일과 클래스 명, 그리고 변수를 UI에 나타내고 싶을때 이렇게 사용한다.

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function Hello(){
-    return <div>안녕하세요</div>
+    const name = "김강열";
+    const style = {
+        backgroundColor : 'aqua',
+        fontSize: 24
+    }
+
+
+    return <div>
+        <div className="itsClassName" style={style}>안녕하세요 {name}님! </div>
+    </div>
 }
 
 export default Hello; //Hello 라는 컴포넌트를 만들어 내보내겠다 라는 의미이다.
