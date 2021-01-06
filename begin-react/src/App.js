5aa729ee42bbfccb0ff4f4f05fa8fb6d1ec5a391
gangyeolkim <kkyy3402@naver.com>
2021-01-06 19:07:19 +0900

    기본 컴포넌트 추가

---
+++ begin-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ begin-react/src/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Hello(){
+    return <div>안녕하세요</div>
+}
+
+export default Hello; //Hello 라는 컴포넌트를 만들어 내보내겠다 라는 의미이다.
